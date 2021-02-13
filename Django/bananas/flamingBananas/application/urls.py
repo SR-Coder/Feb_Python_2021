e60d4fc99db7df1466b0603a56e7792f495dfbd8
SR-Coder <reederje@gmail.com>
2021-02-12 16:56:24 -0800

    afternoon lecture code

--- Django/bananas/flamingBananas/application/urls.py
+++ Django/bananas/flamingBananas/application/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #DISPLAY PATHS
     path('', views.displayIndex),
+    path('hello/<int:myAge>', views.helloAge),
+    path('payments', views.displayPayments),
+    path('success', views.displaySuccess),
+
+    #ACTION PATHS
+    path('hello/sendMeHome', views.sendHome),
+    path('processCard', views.processCard),
 ]
--- Django/bananas/flamingBananas/application/views.py
+++ Django/bananas/flamingBananas/application/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
+#DISPLAY METHODS
+
 def displayIndex(request):
     return HttpResponse('hello world')
+
+def helloAge(request, myAge ):
+    return HttpResponse(f'hello my age is {myAge}')
+
+def displayPayments(request):
+    return render(request, 'index.html')
+
+def displaySuccess(request):
+    return render(request, 'success.html')
+
+#ACTION METHODS
+def sendHome(request):
+    return redirect('/')
+
+def processCard(request):
+    #action
+    print('this is the backend charging your card!!!!')
+    return redirect('/success')
